d90974de77ecced8d24321380ff3eb18d099ac95
Niel Ponciano <nielgp93@gmail.com>
2020-04-17 19:45:55 +0800

    Added initial Home page

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+
+from home import views
+
+app_name = 'home'
+
+urlpatterns = [
+    re_path('^home/?$', views.home, name = "home"),
+    re_path('^index/?$', views.home, name = "index"),
+    path('', views.home, name = "root"),
+
+    re_path('^.*$', views.notFound, name = "notFound"),
+]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hallo, Willkommen!")
+
+def notFound(request):
+    return HttpResponse("\
+        Ooops, tut mir leid!<br>\
+        The resource you are looking for cannot be found.<br>\
+        <br>\
+        Go to <a href='/'>home</a> instead.\
+    ")
