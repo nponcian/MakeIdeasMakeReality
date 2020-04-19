0b9f046f9e619cbc6e17bf91b7bb5e7d21c17a4e
Niel Ponciano <nielgp93@gmail.com>
2020-04-20 01:03:09 +0800

    Enabled changing of listening port through script during setup

--- script/gunicornAndNginxSetup.sh
+++ script/gunicornAndNginxSetup.sh
@@ -8,6 +8,11 @@
 #     bash /absolute/path/from/literally/anywhere/to/script/gunicornAndNginxSetup.sh
 #     or
 #     bash ../relative/path/from/literally/anywhere/to/script/gunicornAndNginxSetup.sh
+# Flags
+#     --changedefault
+#         Replace the Nginx listener to the default port 80 to this web application.
+#         Without this flag, ports used are reset to the original values.
+
 
 PROJECT_PATH=$(readlink -f "${BASH_SOURCE}" | xargs dirname | xargs dirname)
 CONFIG_PATH="${PROJECT_PATH}/config"
@@ -21,13 +26,27 @@ NGINX_CONF="${CONFIG_PATH}/${NGINX_CONF_NAME}"
 SYSTEMD_PATH="/etc/systemd/system/"
 SITES_AVAILABLE_PATH="/etc/nginx/sites-available/"
 SITES_ENABLED_PATH="/etc/nginx/sites-enabled/"
+DEFAULT_AVAILABLE_NGINX_CONF="/etc/nginx/sites-available/default"
+DEFAULT_ENABLED_NGINX_CONF="/etc/nginx/sites-enabled/default"
+
+ORIGINAL_PORT="8000"
+UPDATED_PORT="80"
 
 sudo ln -s ${GUNICORN_SERVICE} ${SYSTEMD_PATH}
 sudo ln -s ${GUNICORN_SOCKET} ${SYSTEMD_PATH}
-systemctl enable --now gunicorn.socket
+sudo systemctl enable --now gunicorn.socket
 
 sudo ln -s ${NGINX_CONF} ${SITES_AVAILABLE_PATH}
 sudo ln -s ${SITES_AVAILABLE_PATH}/${NGINX_CONF_NAME} ${SITES_ENABLED_PATH}
-systemctl enable nginx.service
-systemctl start nginx
-systemctl restart nginx
+sudo systemctl enable nginx.service
+sudo systemctl start nginx
+
+if [[ $# -eq 1 && "${1}" == "--changedefault" ]]; then
+    sed -i 's/listen '${ORIGINAL_PORT}';/listen '${UPDATED_PORT}';/' ${NGINX_CONF}
+    sudo rm -rf ${DEFAULT_ENABLED_NGINX_CONF}
+else
+    sed -i 's/listen '${UPDATED_PORT}';/listen '${ORIGINAL_PORT}';/' ${NGINX_CONF}
+    sudo ln -s ${DEFAULT_AVAILABLE_NGINX_CONF} ${DEFAULT_ENABLED_NGINX_CONF}
+fi
+
+sudo systemctl restart nginx
