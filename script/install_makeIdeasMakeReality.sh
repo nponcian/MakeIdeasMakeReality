fce8fdd6d5f28c1421305fec4cb750892b1e0e2c
Niel Ponciano <nielgp93@gmail.com>
2020-04-20 15:31:17 +0800

    Added automatic installation of virtual environments and pip packages

--- script/install_makeIdeasMakeReality.sh
+++ script/install_makeIdeasMakeReality.sh
@@ -11,15 +11,20 @@ PYTHON3_LOC=$(command -v ${PYTHON3})
 PYTHON3_PIP="python3-pip"
 PYTHON3_VENV="python3-venv"
 PIP3="pip3"
+PIP="pip"
 PIP3_LOC=$(command -v ${PIP3})
+VENV="venv"
+VENV_ACTIVATE="${VENV}/bin/activate"
+
+PIP3_REQUIREMENTS="config/pip3Requirements.txt"
 
 func_printAndExecuteCommand()
 {
     echo "---> Command: ${@}"
-    ${@}
+    eval "${@}" # eval is used to be able to run commands such as redirecting to a file via "">"
 }
 
-func_install()
+func_aptInstall()
 {
     if [[ -z "${1}" ]]; then
         func_printAndExecuteCommand "sudo apt install ${@:2}"
@@ -49,12 +54,17 @@ func_setupGit()
 {
     # If ${GIT_LOC} is empty while ${GIT} isn't:
     #     If everything is enclosed in quotes "", it will be translated to:
-    #         func_install "" value_of_git_here
-    #         - still passing on 2 variables to func_install
+    #         func_aptInstall "" value_of_git_here
+    #         - still passing on 2 variables to func_aptInstall
     #     Else if no quotes:
-    #         func_install value_of_git_here
-    #         - only 1 variable is passed to func_install
-    func_install "${GIT_LOC}" "${GIT}"
+    #         func_aptInstall value_of_git_here
+    #         - only 1 variable is passed to func_aptInstall
+    # This behavior can be easily seen with
+    #     cd $A_NON_EXISTING_VARIABLE
+    #         translates to <cd > which changes directory to home
+    #     cd "$A_NON_EXISTING_VARIABLE"
+    #         translates to <cd ""> which stays on current directory
+    func_aptInstall "${GIT_LOC}" "${GIT}"
 
     func_acceptInput PROJECT_DIR "Enter target path to ${APP_NAME} (e.g. ../../Documents)"
     eval PROJECT_DIR="${PROJECT_DIR}" # Substitutes special character for home "~" to absolute path
@@ -72,8 +82,16 @@ func_setupGit()
 
 func_setupPython()
 {
-    func_install "${PYTHON3_LOC}" "${PYTHON3}"
-    func_install "${PIP3_LOC}" "${PYTHON3_PIP}" "${PYTHON3_VENV}"
+    func_aptInstall "${PYTHON3_LOC}" "${PYTHON3}"
+    func_aptInstall "${PIP3_LOC}" "${PYTHON3_PIP}" "${PYTHON3_VENV}"
+
+    if [[ ! -d "${VENV}" ]]; then
+        func_printAndExecuteCommand "${PYTHON3} -m ${VENV} ${VENV}"
+    fi
+
+    func_printAndExecuteCommand "source ${VENV_ACTIVATE}"
+
+    func_printAndExecuteCommand "${PYTHON3} -m ${PIP} install -r ${PIP3_REQUIREMENTS}"
 }
 
 func_upgradeApt
