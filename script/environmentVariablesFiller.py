0b5ba4e626826eee90d391eedeed0763f2d6548e
Niel Ponciano <nielgp93@gmail.com>
2020-04-21 14:52:06 +0800

    Added option in script to redo the editing of environment variables

--- script/environmentVariablesFiller.py
+++ script/environmentVariablesFiller.py
@@ -17,6 +17,12 @@ TAB = "    "
 INPUT_REQUEST_PREFIX = "---> Input:"
 YES = "y"
 
+def displayEnvVariables():
+    with open(ENVIRONMENT_VARIABLES_FILE) as envVariablesFile:
+        print("Environment variables in", ENVIRONMENT_VARIABLES_FILE)
+        for line in envVariablesFile.readlines():
+            print(TAB + line.strip())
+
 def updateEnvVariables():
     temporaryFileName = ENVIRONMENT_VARIABLES_FILE + "_temp"
     print("Leave blank to retain current value")
@@ -36,20 +42,22 @@ def updateEnvVariables():
 
     os.replace(temporaryFileName, ENVIRONMENT_VARIABLES_FILE)
 
-def displayEnvVariables():
-    with open(ENVIRONMENT_VARIABLES_FILE) as envVariablesFile:
-        print("Environment variables in", ENVIRONMENT_VARIABLES_FILE)
-        for line in envVariablesFile.readlines():
-            print(TAB + line.strip())
-
 displayEnvVariables()
-print("Do you wish to update these environment variables? [y/n]")
+print("You can choose to update", ENVIRONMENT_VARIABLES_FILE, "here or open it with a text editor.")
+print("If you choose to update it here, there is always an option to redo any mistakes made at the end.")
+print("If you choose to update it via a text editor, do it now before responding 'n' below.")
+print("Do you wish to update these environment variables here? [y/n]")
 print(INPUT_REQUEST_PREFIX, end = " ")
 response = input().strip().casefold()
 
-if response == YES:
+if response != YES:
+    print("Environment variables left unchanged in", ENVIRONMENT_VARIABLES_FILE)
+
+while response == YES:
     updateEnvVariables()
     print("Successfully updated environment variables in", ENVIRONMENT_VARIABLES_FILE)
     displayEnvVariables()
-else:
-    print("Environment variables left unchanged in", ENVIRONMENT_VARIABLES_FILE)
+
+    print("Do you need to redo the editing? [y/n]")
+    print(INPUT_REQUEST_PREFIX, end = " ")
+    response = input().strip().casefold()
