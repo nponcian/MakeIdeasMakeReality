31f17a20164d19af8459f9a6d3a188c7e3adb9bb
nponcian <nielgp93@gmail.com>
2020-04-22 17:44:20 +0800

    Added option to change DEBUG mode in script

--- makeIdeasMakeReality/settings.py
+++ makeIdeasMakeReality/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['MIMR_SETTINGS_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['MIMR_SETTINGS_DEBUG'] == "True"
 
 ALLOWED_HOSTS = ['*']
 
