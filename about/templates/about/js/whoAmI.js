d981010290f82f25047ec9ba32231a887725798f
nponcian <nielgp93@gmail.com>
2020-05-07 17:50:10 +0800

    Added jQuery and Youtube API to tech stack

--- about/templates/about/js/whoAmI.js
+++ about/templates/about/js/whoAmI.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    // Slide the carousel upon button click
+    //     .carousel('prev') - Cycles to the previous item
+    //     .carousel('next') - Cycles to the next item.
+    // Reference: https://getbootstrap.com/docs/4.4/components/carousel/#methods
     // $('#moviesCarouselControl #prevMovie').on('click', function(){
     //     $('#moviesCarousel').carousel('prev');
     // });
@@ -10,12 +14,22 @@ $(document).ready(function() {
         $('#moviesCarousel').carousel(targetOperation);
     });
 
+    // When a slide occurs, pause the current iframe video that is playing
+    // player.pauseVideo():Void - Pauses the currently playing video.
+    // Reference: https://developers.google.com/youtube/iframe_api_reference#Playback_controls
     $('#moviesCarousel').on('slide.bs.carousel', function(event) {
+        // The variable "players" contain each Youtube Player for each iframe video
+        // Reference: https://developers.google.com/youtube/iframe_api_reference#Loading_a_Video_Player
+        // event.from - The index of the current video (before the slide occurs)
+        //            - It is also the index of the corresponding player for the current video
+        // Reference: https://getbootstrap.com/docs/4.4/components/carousel/#events
         players[event.from].pauseVideo();
     });
 });
 
-// Start: YouTube Player API Reference for iframe Embeds
+// Start of snippet
+//     YouTube Player API Reference for iframe Embeds
+//     https://developers.google.com/youtube/iframe_api_reference
 // 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
@@ -23,13 +37,13 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-var players = [];
+var players = []; // would contain 1 player for each iframe video
 function onYouTubeIframeAPIReady() {
     var allMovieIframes = document.getElementById("moviesCarousel").getElementsByTagName('iframe');
     for (currentIFrame of allMovieIframes)
     {
         players.push(new YT.Player(
-            currentIFrame.id,
+            currentIFrame.id, // the target iframe video
             { events: { 'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange } }));
     }
 }
@@ -41,19 +55,32 @@ function onPlayerReady(event) {
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
 // var done = false;
-function onPlayerStateChange(event) {
+function onPlayerStateChange(event) { // triggered everytime ANY iframe video player among the "players" list is played, paused, ended, etc.
     // if (event.data == YT.PlayerState.PLAYING && !done) {
     //     setTimeout(stopVideo, 6000);
     //     done = true;
+    // Check if any iframe video is being played (or is currently buffering to be played)
+    // Reference: https://developers.google.com/youtube/iframe_api_reference#Events
     if (event.data == YT.PlayerState.PLAYING || event.data == YT.PlayerState.BUFFERING) {
+        // If any player has been detected to be currently playing or buffering, pause the carousel from sliding
+        // .carousel('pause') - Stops the carousel from cycling through items.
+        // Reference: https://getbootstrap.com/docs/4.4/components/carousel/#methods
         $('#moviesCarousel').carousel('pause');
     }
     else
     {
+        // If there are no currently playing nor buffering videos, resume the sliding of the carousel.
+        // This means that once the current video is in a state that is not playing (aside from buffering), either it was:
+        //     1. paused intentionally
+        //     2. paused as an effect of a slide
+        //     3. video has ended
+        //     4. wasn't totally played from the start
+        //     5. and literally any form where the video timer isn't running ;)
+        //     - then the carousel would now resume sliding.
         $('#moviesCarousel').carousel();
     }
 }
 function stopVideo() {
     // player.stopVideo();
 }
-// End: YouTube Player API Reference for iframe Embeds
+// End of snippet from Youtube iframe API
