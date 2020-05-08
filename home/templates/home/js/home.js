97f2e180a588d450c34904c13085b4ffbc1d1351
nponcian <nielgp93@gmail.com>
2020-05-08 21:44:18 +0800

    Added more languages to home page welcome greetings

--- home/templates/home/js/home.js
+++ home/templates/home/js/home.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
 
-$("#welcomeMessage").html(welcomeGreeting);
+$("#welcomeMessage").html(initialWelcome);
 // or
-// document.getElementById('welcomeMessage').innerHTML = welcomeGreeting;
+// document.getElementById('welcomeMessage').innerHTML = initialWelcome;
 
 $('#welcomeMessage').hover(
     function() {
-        languageTracker = languageTracker % welcomeLanguages.length;
-        $(this).html(welcomeLanguages[languageTracker]);
-        ++languageTracker;
+        greetingsTracker %= greetings.length;
+        $(this).html(greetings[greetingsTracker]);
+        ++greetingsTracker;
+
+        if (!isInitialWelcomeUpdated && greetingsTracker >= greetings.length)
+        {
+            initialWelcome =  initialWelcomeUpdated;
+            isInitialWelcomeUpdated = true;
+        }
     },
     function() {
-        $(this).html(welcomeGreeting);
+        $(this).html(initialWelcome);
     });
 
 });
 
-var languageTracker = 0
-var welcomeGreeting = "Hey! Surprises are coming when you hover in and out of here!";
-var welcomeLanguages = [
-// var englishWelcome =
+var initialWelcome = "Hey! Surprises are coming when you hover in and out of here!";
+var initialWelcomeUpdated = "No more surprises!";
+var isInitialWelcomeUpdated = false;
+var greetingsTracker = 0
+var greetings = [
+    // English
     '\
         <p>\
             Ready to pay nothing? Navigate to the\
@@ -27,19 +35,26 @@ var welcomeLanguages = [
             now!\
         </p>\
     ',
-// var espanolWelcome =
+    // Espanol
     '\
         <p>\
-            ¿Estas lista para no pagar nada? Navega a los\
-            <a href="/service/" class="border-bottom border-top border-warning text-warning">servicios</a>\
-            ahora!\
+            ¿Estas lista para pagar nada? Obtenga mas informacion sobre el\
+            <a href="/about/mimr/" class="border-bottom border-top border-warning text-warning">proyecto</a>!\
         </p>\
     ',
-// var deutschWelcome =
+    // Deutsch
     '\
         <p>\
-            Bist du bereit nichts zu bezahlen? Navigieren Sie jetzt zu den \
-            <a href="/service/" class="border-bottom border-top border-warning text-warning">Diensten</a>!\
+            Bist du bereit nichts zu bezahlen? Erfahren Sie mehr uber\
+            <a href="/about/whoami/" class="border-bottom border-top border-warning text-warning">uns</a>!\
         </p>\
-    '
+    ',
+    // Tagalog
+    '\
+        <p>\
+            Handa ka na bang hindi magbayad? Alamin ang\
+            <a href="https://github.com/nponcian/makeIdeasMakeReality" class="border-bottom border-top border-warning text-warning">algoritmo</a>\
+            sa likod ng pagina na ito!\
+        </p>\
+    ',
 ];
