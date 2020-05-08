7b49b89692b49e1fd2b3e73af434c99d760bd767
nponcian <nielgp93@gmail.com>
2020-05-08 23:21:51 +0800

    Added flags to welcome messages

--- home/templates/home/js/home.js
+++ home/templates/home/js/home.js
@@ -7,7 +7,12 @@ $("#welcomeMessage").html(initialWelcome);
 $('#welcomeMessage').hover(
     function() {
         greetingsTracker %= greetings.length;
-        $(this).html(greetings[greetingsTracker]);
+
+        var htmlContent = greetings[greetingsTracker][0];
+        var bgImage = greetings[greetingsTracker][1];
+        $(this).html(htmlContent);
+        $("#welcomeMessage").css('background-image', 'url("' + bgImage + '")');
+        $("#welcomeMessage").css('background-size', 'contain'); // fit flags and repeat
         ++greetingsTracker;
 
         if (!isInitialWelcomeUpdated && greetingsTracker >= greetings.length)
@@ -18,6 +23,7 @@ $('#welcomeMessage').hover(
     },
     function() {
         $(this).html(initialWelcome);
+        $("#welcomeMessage").css('background-image', 'url("")');
     });
 
 });
@@ -27,34 +33,42 @@ var initialWelcomeUpdated = "No more surprises!";
 var isInitialWelcomeUpdated = false;
 var greetingsTracker = 0
 var greetings = [
-    // English
-    '\
-        <p>\
-            Ready to pay nothing? Navigate to the\
-            <a href="/service/" class="border-bottom border-top border-warning text-warning">services</a>\
-            now!\
-        </p>\
-    ',
-    // Espanol
-    '\
-        <p>\
-            ¿Estas lista para pagar nada? Obtenga mas informacion sobre el\
-            <a href="/about/mimr/" class="border-bottom border-top border-warning text-warning">proyecto</a>!\
-        </p>\
-    ',
-    // Deutsch
-    '\
-        <p>\
-            Bist du bereit nichts zu bezahlen? Erfahren Sie mehr uber\
-            <a href="/about/whoami/" class="border-bottom border-top border-warning text-warning">uns</a>!\
-        </p>\
-    ',
-    // Tagalog
-    '\
-        <p>\
-            Handa ka na bang hindi magbayad? Alamin ang\
-            <a href="https://github.com/nponcian/makeIdeasMakeReality" class="border-bottom border-top border-warning text-warning">algoritmo</a>\
-            sa likod ng pagina na ito!\
-        </p>\
-    ',
+    [ // English
+        '\
+            <p class="bg-secondary p-2">\
+                Ready to pay nothing? Navigate to the\
+                <a href="/service/" class="border-bottom border-top border-warning text-warning">services</a>\
+                now!\
+            </p>\
+        ',
+        "https://upload.wikimedia.org/wikipedia/en/a/ae/Flag_of_the_United_Kingdom.svg"
+    ],
+    [ // Espanol
+        '\
+            <p class="bg-secondary p-2">\
+                ¿Estas lista para pagar nada? Obtenga mas informacion sobre el\
+                <a href="/about/mimr/" class="border-bottom border-top border-warning text-warning">proyecto</a>!\
+            </p>\
+        ',
+        "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/1200px-Flag_of_Spain.svg.png"
+    ],
+    [ // Deutsch
+        '\
+            <p class="bg-secondary p-2">\
+                Bist du bereit nichts zu bezahlen? Erfahren Sie mehr uber\
+                <a href="/about/whoami/" class="border-bottom border-top border-warning text-warning">uns</a>!\
+            </p>\
+        ',
+        "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png"
+    ],
+    [ // Tagalog
+        '\
+            <p class="bg-secondary p-2">\
+                Handa ka na bang hindi magbayad? Alamin ang\
+                <a href="https://github.com/nponcian/makeIdeasMakeReality" class="border-bottom border-top border-warning text-warning">algoritmo</a>\
+                sa likod ng pagina!\
+            </p>\
+        ',
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Flag_of_the_Philippines.svg/1200px-Flag_of_the_Philippines.svg.png"
+    ],
 ];
